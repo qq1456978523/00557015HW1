e3b6b851247e43b89b919ed8edad971a6e8417f0
c8610290927 <c8610290927@gmail.com>
2020-01-08 10:22:29 +0800

    Initial Commit

---
+++ 15-hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  15-hw1
+//
+//  Created by User20 on 2020/1/8.
+//  Copyright © 2020 000. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
